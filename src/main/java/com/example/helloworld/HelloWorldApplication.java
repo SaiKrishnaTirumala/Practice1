c772efad736e165c839a9f5cf8b4cba37ce2cfff
SaiKrishnaTirumala <125542725+SaiKrishnaTirumala@users.noreply.github.com>
2023-11-11 00:26:51 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/example/helloworld/HelloWorldApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
+@ComponentScan({"com.mypackage.controller"})
 public class HelloWorldApplication {
 
     public static void main(String[] args) {
